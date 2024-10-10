0f596f8fabe05f5fadacfdb0526b6a36e7bcb995
Felix <eudaimov@gmail.com>
2024-10-10 10:08:51 +0200

    Route change

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,28 @@
-import {createRouter, createWebHistory} from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory } from 'vue-router';
 
-export const rutas= [
+export const rutas = [
     'Home',
     'Plantilla'
 ];
 
-const vistas = [];
-vistas.push({path:`/`, component: import(`../views/Home.vue`)})
+const rutasDinamicas = {};
 for (const ruta of rutas) {
-    vistas.push(
-        {path:`/${ruta}`, component: import(`../views/${ruta}.vue`)}
-    );
+    rutasDinamicas[`../views/${ruta}.vue`] = import(`../views/${ruta}.vue`);
 }
-console.info(vistas)
+
+const vistas = [
+    { path: '/', component: () => import('../views/Home.vue') },
+    ...Object.keys(rutasDinamicas).map(key => ({
+        path: `/${key.replace('../views/', '').replace('.vue', '')}`,
+        component: () => rutasDinamicas[key]
+    }))
+];
+
+console.info(vistas);
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes:[
-    ...vistas,
-]}
-)
+    routes: vistas
+});
 
 export default router;
