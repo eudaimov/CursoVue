47a3bb614d3038fa454195e082798682738e48f8
Félix García Sánchez <eudaimov@gmail.com>
2025-06-13 10:32:18 +0200

    Configuracion base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/CursoVue/', // Reemplaza con el nombre de tu repositorio
+  //base: '/CursoVue/', // Reemplaza con el nombre de tu repositorio
+  base: '/',
   build: {
     outDir: 'public', // Cambia la carpeta de salida a 'public'
   }
